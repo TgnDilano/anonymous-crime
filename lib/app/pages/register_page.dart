b025af3a96d51a561e68c72c7d653586adb31aba
donalawa <awadonalcien12@gmail.com>
2023-11-17 11:30:27 +0100

    update

---
+++ lib/app/pages/register_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:macos_ui_app/app/constants/colors.dart';
+import 'package:macos_ui_app/app/widget/inputs/input_field.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _nameController = TextEditingController();
+    TextEditingController _passwordCodeController = TextEditingController();
+
+    void loginUser() {
+      print(_nameController.text);
+      print(_passwordCodeController.text);
+    }
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Welcome Back", style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                        ),
+                        ),
+                        SizedBox(height: 5,),
+                        Text("Welcome to stealth. your security is our priority", style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.normal
+                        ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                NInputField(
+                  controller: _nameController,
+                  hintText: 'Name',
+                  onChange: (str) {
+                    // _nameController.value = str;
+                  },
+                ),
+                NInputField(
+                  controller: _nameController,
+                  hintText: 'Email',
+                  onChange: (str) {
+                    // _nameController.value = str;
+                  },
+                ),
+                NInputField(
+                  hintText: 'Passoword',
+                  isPwd: true,
+                  controller: _passwordCodeController,
+                  onChange: (str) {},
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: loginUser,
+                    style: ElevatedButton.styleFrom(
+                      primary: AppColors.kPrimary,
+                      onPrimary: Colors.white,
+
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                    child: Text('Login'),
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(fontSize: 13),
+                    ),
+                    SizedBox(height: 8),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushNamed('');
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: AppColors.kPrimary,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
